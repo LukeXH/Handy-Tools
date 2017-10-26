c5e3853f940262881737b20e3162ef7b56dc7e30
Lukepahill <luke.pa.hill@gmail.com>
2017-10-25 17:01:04 -0700

    Modded!

--- PDFs/Gaussian.m
+++ PDFs/Gaussian.m
@@ -5,73 +5,58 @@ classdef Gaussian < handle
 %   An Gaussian object can be created by passing the constructor the following:
 %   Gaussian: assumed 1D, normal distribution, mean = 0, std = 1
 %   Gaussian(u, s): A 1D normal distribution, mean = u, std = s
-%   Gaussian(u, s, x): A 1D normal distribution, mean = u, std = s,
-%       returns the value for the evaluation at x
 %   Gaussian(u = nx1, s = nx1): A nD normal dist, mean = u, std = s
-%   Gaussian(u = nx1, s = nx1, x = mx1): A nD normal dist, returns the
-%       value for the evaluation for all the elements in vector x
-%   Gaussian([], [], [], option): option is a string, defaulted to 'Normal'
+%   Gaussian([], [], option): option is a string, defaulted to 'Normal'
 %       that selects if the Gaussian is 'Normal', 'Clipped', or 'Folded'
 
-
+%    Gaussian(u, s, x): A 1D normal distribution, mean = u, std = s,
+%       returns the value for the evaluation at x
+%   Gaussian(u = nx1, s = nx1, x = mx1): A nD normal dist, returns the
+%       value for the evaluation for all the elements in vector x
     
     properties
         dim %Gaussian dimension
         u   %Gaussian mean
         s   %Gaussian standard deviation or covariance matrix
+        distribution_type % What type of distribution
     end
     
     methods
         function this = Gaussian(varargin)
-            switch nargin
-                case 0
-                    this.dim = 1;
-                    this.u = 0;
-                    this.s = 1;
-                case 2  % No immediate evaluation
-                    u = varargin{1};
-                    s = varargin{2};
-                    if max(size(u)) == 1 && max(size(s)) == 1 % 1D case
-                       this.dim = 1;
-                       this.u = u;
-                       this.s = s;
-                    elseif size(u,1) > 1 && size(s,1) > 1 && size(u,1) == size(s,1) && size(s,1) == size(s,2)
-                        this.dim = size(u,1);
-                        this.u = u;
-                        this.s = s;
-                    elseif size(u,2) > 1 && size(s,1) > 1 && size(u,2) == size(s,1) && size(s,1) == size(s,2)
-                        this.dim = size(u,2);
-                        this.u = u.';    % Flip vertical
-                        this.s = s.';    % Flip vertical
-                    else
-                        error('Dimension mismatch on inputs')
-                    end
-                case 3  % Immediate evaluation
-                    u = varargin{1};
-                    s = varargin{2};
-                    x = varargin{3};
-                    if max(size(u)) == 1 && max(size(s)) == 1 % 1D case
-                       this.dim = 1;
-                       this.u = u;
-                       this.s = s;
-                    elseif size(u,1) > 1 && size(s,1) > 1 && size(u,1) == size(s,1) && size(s,1) == size(s,2)
-                        this.dim = size(u,1);
-                        this.u = u;
-                        this.s = s;
-                    elseif size(u,2) > 1 && size(s,1) > 1 && size(u,2) == size(s,1) && size(s,1) == size(s,2)
-                        this.dim = size(u,2);
-                        this.u = u.';    % Flip vertical
-                        this.s = s.';    % Flip vertical
-                    else
-                        error('Dimension mismatch on inputs')
-                    end
-            %GAUSSIAN Construct an instance of this class
-            %   Detailed explanation goes here
-                otherwise
-                    class(varargin)
-                    size(varargin)
-                    error('Unknown argument to Gaussian constructor of type and size listed above')
-            end
+            if nargin == 0 % Default
+                this.dim = 1;
+                this.u = 0;
+                this.s = 1;
+                this.distribution_type = 'Normal';
+            elseif nargin >=2
+                u = varargin{1};
+                s = varargin{2};
+                if max(size(u)) == 1 && max(size(s)) == 1 % 1D case
+                   this.dim = 1;
+                   this.u = u;
+                   this.s = s;
+                elseif size(u,1) > 1 && size(s,1) > 1 && size(u,1) == size(s,1) && size(s,1) == size(s,2)
+                    this.dim = size(u,1);
+                    this.u = u;
+                    this.s = s;
+                elseif size(u,2) > 1 && size(s,1) > 1 && size(u,2) == size(s,1) && size(s,1) == size(s,2)
+                    this.dim = size(u,2);
+                    this.u = u.';    % Flip vertical
+                    this.s = s.';    % Flip vertical
+                else
+                    error('Dimension mismatch on inputs')
+                end
+                % Set distribution type
+                if nargin == 3
+                    this.distribution_type = varargin{3};
+                else
+                    this.distribution_type = 'Normal';
+                end
+            else
+                class(varargin)
+                size(varargin)
+                error('Unknown argument to Gaussian constructor of type and size listed above')
+            end% End if 
         end
         
         function outputArg = method1(obj,inputArg)
